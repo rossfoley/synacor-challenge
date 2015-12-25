47112b6e4d6cecaf04e3a40b58fb2194ab5c31fa
Ross Foley <ross3141@gmail.com>
2015-12-24 22:24:51 -0800

    Indenting

--- op_code_interpreter.rb
+++ op_code_interpreter.rb
@@ -1,45 +1,45 @@
 class OpCodeInterpreter
-  def initialize
-  	@op_codes = 
-    {0 => {args: 0, op_name: 'halt', name: 'Halt'}, 
-		 1 => {args: 2, op_name: 'set',  name: 'Set'},
-		 2 => {args: 1, op_name: 'push', name: 'Push'},
-		 3 => {args: 1, op_name: 'pop',  name: 'Pop'},
-		 4 => {args: 3, op_name: 'eq',   name: 'Equals'},
-		 5 => {args: 3, op_name: 'gt',   name: 'Greater Than'},
-		 6 => {args: 1, op_name: 'jmp',  name: 'Jump'},
-		 7 => {args: 2, op_name: 'jt',   name: 'Jump If Not Zero'},
-		 8 => {args: 2, op_name: 'jf',   name: 'Jump If Zero'},
-		 9 => {args: 3, op_name: 'add',  name: 'Add'},
-		10 => {args: 3, op_name: 'mult', name: 'Multiply'},
-		11 => {args: 3, op_name: 'mod',  name: 'Modulo'},
-		12 => {args: 3, op_name: 'and',  name: 'And'},
-		13 => {args: 3, op_name: 'or',   name: 'Or'},
-		14 => {args: 2, op_name: 'not',  name: 'Not'},
-		15 => {args: 2, op_name: 'rmem', name: 'Read Memory'},
-		16 => {args: 2, op_name: 'wmem', name: 'Write Memory'},
-		17 => {args: 1, op_name: 'call', name: 'Call'},
-		18 => {args: 0, op_name: 'ret',  name: 'Return'},
-		19 => {args: 1, op_name: 'out',  name: 'Output'},
-		20 => {args: 1, op_name: 'in',   name: 'Input'},
-  	21 => {args: 0, op_name: 'nop',  name: 'No Operation'}}
-  end
+	def initialize
+		@op_codes =
+				{0 => {args: 0, op_name: 'halt', name: 'Halt'},
+				 1 => {args: 2, op_name: 'set',  name: 'Set'},
+				 2 => {args: 1, op_name: 'push', name: 'Push'},
+				 3 => {args: 1, op_name: 'pop',  name: 'Pop'},
+				 4 => {args: 3, op_name: 'eq',   name: 'Equals'},
+				 5 => {args: 3, op_name: 'gt',   name: 'Greater Than'},
+				 6 => {args: 1, op_name: 'jmp',  name: 'Jump'},
+				 7 => {args: 2, op_name: 'jt',   name: 'Jump If Not Zero'},
+				 8 => {args: 2, op_name: 'jf',   name: 'Jump If Zero'},
+				 9 => {args: 3, op_name: 'add',  name: 'Add'},
+				 10 => {args: 3, op_name: 'mult', name: 'Multiply'},
+				 11 => {args: 3, op_name: 'mod',  name: 'Modulo'},
+				 12 => {args: 3, op_name: 'and',  name: 'And'},
+				 13 => {args: 3, op_name: 'or',   name: 'Or'},
+				 14 => {args: 2, op_name: 'not',  name: 'Not'},
+				 15 => {args: 2, op_name: 'rmem', name: 'Read Memory'},
+				 16 => {args: 2, op_name: 'wmem', name: 'Write Memory'},
+				 17 => {args: 1, op_name: 'call', name: 'Call'},
+				 18 => {args: 0, op_name: 'ret',  name: 'Return'},
+				 19 => {args: 1, op_name: 'out',  name: 'Output'},
+				 20 => {args: 1, op_name: 'in',   name: 'Input'},
+				 21 => {args: 0, op_name: 'nop',  name: 'No Operation'}}
+	end
 
-  def num_args(op)
-    code = @op_codes[op]
-    return 0 if code.nil?
-    code[:args]
-  end
+	def num_args(op)
+		code = @op_codes[op]
+		return 0 if code.nil?
+		code[:args]
+	end
 
-  def name(op)
-  	code = @op_codes[op]
-    return '' if code.nil?
-    code[:name]
-  end
+	def name(op)
+		code = @op_codes[op]
+		return '' if code.nil?
+		code[:name]
+	end
 
-  def op_name(op)
-  	code = @op_codes[op]
-    return '' if code.nil?
-    code[:op_name]
+	def op_name(op)
+		code = @op_codes[op]
+		return '' if code.nil?
+		code[:op_name]
 	end
 end
--- virtual_machine.rb
+++ virtual_machine.rb
@@ -1,4 +1,4 @@
-require 'debugger'
+require 'byebug'
 require 'highline/system_extensions'
 require_relative 'op_code_interpreter'
 include HighLine::SystemExtensions
@@ -6,12 +6,12 @@ include HighLine::SystemExtensions
 class VirtualMachine
 	def initialize
 		@stack = []
-  	@registers = [0] * 8
+		@registers = [0] * 8
 		@instructions = []
 		@running = true
 		@pc = 0
 		@op_code = OpCodeInterpreter.new
-    $stdout.sync = true
+		$stdout.sync = true
 	end
 
 	def halt
@@ -34,109 +34,109 @@ class VirtualMachine
 
 	def run_op(instruction, args)
 		case instruction
-		when 0
-			@running = false
-
-		when 1
-			set_register(args[0], process_arg(args[1]))
-
-		when 2
-			@stack.push(process_arg(args[0]))
-
-		when 3
-			popped = @stack.pop
-			unless popped.nil?
-				set_register(args[0], popped)
-			else
-				puts "Error! Stack was empty during pop!"
-			end
-
-		when 4
-			if process_arg(args[1]) == process_arg(args[2])
-				set_register(args[0], 1)
-			else
-				set_register(args[0], 0)
-			end
-
-		when 5
-			if process_arg(args[1]) > process_arg(args[2])
-				set_register(args[0], 1)
-			else
-				set_register(args[0], 0)
-			end
-
-		when 6
-			@pc = process_arg(args[0])
-
-		when 7
-			if process_arg(args[0]) != 0
-				location = process_arg(args[1])
-				@pc = location
-			end
-
-		when 8
-			if process_arg(args[0]) == 0
-				location = process_arg(args[1])
-				@pc = location
-			end
-
-		when 9
-			sum = (process_arg(args[1]) + process_arg(args[2])) % 32768
-			set_register(args[0], sum)
-
-		when 10
-			sum = (process_arg(args[1]) * process_arg(args[2])) % 32768
-			set_register(args[0], sum)
-
-		when 11
-			sum = (process_arg(args[1]) % process_arg(args[2]))
-			set_register(args[0], sum)
-
-		when 12
-			sum = (process_arg(args[1]) & process_arg(args[2]))
-			set_register(args[0], sum)
-
-		when 13
-			sum = (process_arg(args[1]) | process_arg(args[2]))
-			set_register(args[0], sum)
-
-		when 14
-			set_register(args[0], 32767 - process_arg(args[1]))
-
-		when 15
-			memory = @instructions[process_arg(args[1])]
-			set_register(args[0], memory)
-
-		when 16
-			@instructions[process_arg(args[0])] = process_arg(args[1])
-
-		when 17
-			@stack.push @pc
-			@pc = process_arg(args[0])
-
-		when 18
-			value = @stack.pop
-			if value.nil?
+			when 0
 				@running = false
+
+			when 1
+				set_register(args[0], process_arg(args[1]))
+
+			when 2
+				@stack.push(process_arg(args[0]))
+
+			when 3
+				popped = @stack.pop
+				unless popped.nil?
+					set_register(args[0], popped)
+				else
+					puts "Error! Stack was empty during pop!"
+				end
+
+			when 4
+				if process_arg(args[1]) == process_arg(args[2])
+					set_register(args[0], 1)
+				else
+					set_register(args[0], 0)
+				end
+
+			when 5
+				if process_arg(args[1]) > process_arg(args[2])
+					set_register(args[0], 1)
+				else
+					set_register(args[0], 0)
+				end
+
+			when 6
+				@pc = process_arg(args[0])
+
+			when 7
+				if process_arg(args[0]) != 0
+					location = process_arg(args[1])
+					@pc = location
+				end
+
+			when 8
+				if process_arg(args[0]) == 0
+					location = process_arg(args[1])
+					@pc = location
+				end
+
+			when 9
+				sum = (process_arg(args[1]) + process_arg(args[2])) % 32768
+				set_register(args[0], sum)
+
+			when 10
+				sum = (process_arg(args[1]) * process_arg(args[2])) % 32768
+				set_register(args[0], sum)
+
+			when 11
+				sum = (process_arg(args[1]) % process_arg(args[2]))
+				set_register(args[0], sum)
+
+			when 12
+				sum = (process_arg(args[1]) & process_arg(args[2]))
+				set_register(args[0], sum)
+
+			when 13
+				sum = (process_arg(args[1]) | process_arg(args[2]))
+				set_register(args[0], sum)
+
+			when 14
+				set_register(args[0], 32767 - process_arg(args[1]))
+
+			when 15
+				memory = @instructions[process_arg(args[1])]
+				set_register(args[0], memory)
+
+			when 16
+				@instructions[process_arg(args[0])] = process_arg(args[1])
+
+			when 17
+				@stack.push @pc
+				@pc = process_arg(args[0])
+
+			when 18
+				value = @stack.pop
+				if value.nil?
+					@running = false
+				else
+					@pc = value
+				end
+
+			when 19
+				print process_arg(args[0]).chr
+
+			when 20
+				char = get_character
+				if char == 37
+					byebug
+				end
+				set_register(args[0], char)
+
+			when 21
+
 			else
-				@pc = value
-			end
-
-		when 19
-			print process_arg(args[0]).chr
-
-		when 20
-			char = get_character
-			if char == 37
-				debugger
-			end
-			set_register(args[0], char)
-
-		when 21
-			
-		else
-			puts "Unknown instruction! OpCode: #{instruction}, Instruction: #{@op_code.name(instruction)}, Args: #{args}"
-			@running = false
+				puts "Unknown instruction! OpCode: #{instruction}, Instruction: #{@op_code.name(instruction)}, Args: #{args}"
+				@running = false
 		end
 	end
 
